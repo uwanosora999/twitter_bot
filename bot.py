6c498a2950a03077b5a8a4981f7e924836c7792d
uwanosora999 <hatayama-sora@g.ecc.u-tokyo.ac.jp>
2021-02-11 20:10:00 +0900

    First commit

---
+++ bot.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+import requests
+import linecache
+import tweepy
+import os
+
+def puttweet():
+    #from requests_oauthlib import OAuth1Session
+    CK=os.environ["CONSUMER_KEY"]
+    CS=os.environ["CONSUMER_SECRET"]
+    AT=os.environ["ACCESS_TOKEN_KEY"]
+    AS=os.environ["ACCESS_TOKEN_SECRET"]
+
+    auth = tweepy.OAuthHandler(CK, CS)
+    auth.set_access_token(AT, AS)
+
+    api = tweepy.API(auth)
+
+    status = api.mentions_timeline(count = 1)
+    #status = api.user_timeline(id='uwanosora999',count = 1)
+
+    for mention in status:
+        reply_text = "@"+str(mention.user.screen_name)+" "+"呼吸できて偉い"
+        api.update_status(status = reply_text, in_reply_to_status_id = mention.id)
+        api.create_favorite(mention.id)
---
+++ clock.py
@@ -0,0 +1,11 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
+import words
+
+twische = BlockingScheduler()
+
+@twische.scheduled_job('interval',minutes=1)
+def timed_job():
+    bot.puttweet()
+
+if __name__ == "__main__":
+    twische.start()
