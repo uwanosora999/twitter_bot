080c2f3cc93d3639b99112a1b72ddba1dbe16cda
uwanosora999 <hatayama-sora@g.ecc.u-tokyo.ac.jp>
2021-02-11 23:56:59 +0900

    Add string (@) check module

--- bot.py
+++ bot.py
@@ -10,35 +10,38 @@ def tweet():
     CS=os.environ["CONSUMER_SECRET"]
     AT=os.environ["ACCESS_TOKEN_KEY"]
     AS=os.environ["ACCESS_TOKEN_SECRET"]
+    AUTH=os.environ["AUTHOR"]
 
     auth = tweepy.OAuthHandler(CK, CS)
     auth.set_access_token(AT, AS)
 
     api = tweepy.API(auth)
 
-    status = api.user_timeline(id='uwanosora999',count = 5)
+    status = api.user_timeline(id=AUTH,count = 5)
 
     for mention in status:
-        reply_text = "@"+str(mention.user.screen_name)+" "+"呼吸できて偉い"
-        api.update_status(status = reply_text, in_reply_to_status_id = mention.id)
-        api.create_favorite(mention.id)
+        if '@' in mention.text:
+            pass
+        else:
+            reply_text = "@"+str(mention.user.screen_name)+" "+"呼吸できて偉い"
+            api.update_status(status = reply_text, in_reply_to_status_id = mention.id)
+            api.create_favorite(mention.id)
 
 def reply():
-    #from requests_oauthlib import OAuth1Session
     CK=os.environ["CONSUMER_KEY"]
     CS=os.environ["CONSUMER_SECRET"]
     AT=os.environ["ACCESS_TOKEN_KEY"]
     AS=os.environ["ACCESS_TOKEN_SECRET"]
+    AUTH=os.environ["AUTHOR"]
 
     auth = tweepy.OAuthHandler(CK, CS)
     auth.set_access_token(AT, AS)
 
     api = tweepy.API(auth)
 
-    status = api.mentions_timeline(count = 5)
-
+    status = api.mentions_timeline(count = 10) 
     for mention in status:
-        if mantion.id == 'uwanosora999':
+        if mention.user.screen_name == AUTH:
             reply_text = "@"+str(mention.user.screen_name)+" "+"リプライできて偉い"
         else:
             reply_text = "@"+str(mention.user.screen_name)+" "+"リプライなんて誰でもできる"
@@ -46,9 +49,6 @@ def reply():
         api.create_favorite(mention.id)
 
 
-
-
-
 if __name__ == "__main__":
     tweet()
-    reply()
+    #reply()
